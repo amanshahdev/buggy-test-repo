2fa9f38968749a96e4e9b9ca14400430404a5c99
Aman Shah <amnshh216@gmail.com>
2026-08-24 11:07:46 +0545

    first commit

---
+++ buggy.js
@@ -0,0 +1,62 @@
+// buggy.js - intentionally buggy JS file for testing purposes
+
+// Bug 1: off-by-one error in loop (skips last element / goes out of bounds depending on use)
+function sumArray(arr) {
+  let total = 0;
+  for (let i = 0; i <= arr.length; i++) {
+    total += arr[i];
+  }
+  return total;
+}
+
+// Bug 2: using == instead of === causes type coercion issues
+function isEqual(a, b) {
+  return a == b;
+}
+
+// Bug 3: classic var-in-loop closure bug
+function createCounters() {
+  var counters = [];
+  for (var i = 0; i < 3; i++) {
+    counters.push(function () {
+      return i;
+    });
+  }
+  return counters;
+}
+
+// Bug 4: missing await, function returns a pending Promise instead of the value
+async function fetchData(url) {
+  const result = fetch(url).then(res => res.json());
+  return result;
+}
+
+// Bug 5: mutating an array while iterating over it with forEach
+function removeEvens(arr) {
+  arr.forEach((num, index) => {
+    if (num % 2 === 0) {
+      arr.splice(index, 1);
+    }
+  });
+  return arr;
+}
+
+// Bug 6: no check for division by zero
+function divide(a, b) {
+  return a / b;
+}
+
+// Bug 7: typo referencing an undeclared variable
+function greet(name) {
+  console.log("Hello, " + nam);
+}
+
+module.exports = {
+  sumArray,
+  isEqual,
+  createCounters,
+  fetchData,
+  removeEvens,
+  divide,
+  greet
+};
